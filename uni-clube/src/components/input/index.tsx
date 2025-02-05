bdcbf1608c9e09c8a5f2f919d2cd10bebbe7cb87
Manrick-Lazaro <manricksantos@gmail.com>
2025-02-05 18:01:25 -0300

    "feat: cria componente de input."

---
+++ uni-clube/src/components/input/index.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import type InputProps from "../../types/components/inputProps";
+import { JSX } from "react";
+import { IoSearch } from "react-icons/io5";
+
+export function Input(props: InputProps): JSX.Element {
+  return (
+    <div className="relative w-full">
+      <input
+        {...props}
+        className="rounded-[30px] h-[54px] w-full focus:outline-none focus:ring-1 pl-5 drop-shadow-md"
+      />
+      <IoSearch className="mb-2 text-gray-400 w-6 h-6 absolute right-5 top-4 " />
+    </div>
+  );
+}

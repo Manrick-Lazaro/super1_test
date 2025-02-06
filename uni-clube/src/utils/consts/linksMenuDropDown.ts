5c03a538155365c1b23d00f1a369ba93615eb7ce
Manrick-Lazaro <manricksantos@gmail.com>
2025-02-06 15:55:17 -0300

    feat: adiciona Links do menu no utilitarios.

---
+++ uni-clube/src/utils/consts/linksMenuDropDown.ts
@@ -0,0 +1,26 @@
+export const linksSaude = [
+  { label: "Beleza", href: "/" },
+  { label: "Cuidados Pessoais", href: "/" },
+  { label: "Esporte", href: "/" },
+  { label: "Cremes", href: "/" },
+];
+
+export const linksEstilo = [
+  { label: "Vestuario", href: "/" },
+  { label: "Calçados", href: "/" },
+  { label: "Joias", href: "/" },
+  { label: "Luvas", href: "/" },
+  { label: "Chapeus", href: "/" },
+];
+
+export const linksDepartamentos = [
+  { label: "Funerária", href: "/" },
+  { label: "Informática", href: "/" },
+  { label: "Comércio", href: "/" },
+  { label: "Ifoood", href: "/" },
+  { label: "Luva de pedreiro", href: "/" },
+  { label: "Football", href: "/" },
+  { label: "Carro", href: "/" },
+  { label: "Moto", href: "/" },
+  { label: "Mangá", href: "/" },
+];

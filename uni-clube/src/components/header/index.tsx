cf8615300a3d542687b6f6a55fbffa3a6800db5e
Manrick-Lazaro <manricksantos@gmail.com>
2025-02-05 23:03:17 -0300

    feat: importa o uso do navbar no header.

--- uni-clube/src/components/header/index.tsx
+++ uni-clube/src/components/header/index.tsx
@@ -8,6 +8,7 @@ import { LuShoppingCart } from "react-icons/lu";
 
 import { Input } from "../input";
 import { CircleButton } from "../circleButton";
+import { NavBar } from "../navbar";
 
 export function Header(): JSX.Element {
   const [search, setSearch] = useState("");
@@ -31,55 +32,66 @@ export function Header(): JSX.Element {
   return (
     <div>
       <div className="h-[8px] w-full bg-[#F47920]"></div>
-      <header className="h-[122px] flex justify-center items-center bg-[#EAE0D5]">
-        <div className="wrapper flex flex-row justify-center items-center gap-[30px] ">
-          <div className="flex justify-center items-center h-24">
-            <Image
-              alt="Logo dae Uniclube"
-              src="/images/Logo.png"
-              height={100}
-              width={200}
-            />
+      <header>
+        <div className="h-[122px] flex justify-center items-center bg-[#EAE0D5]">
+          <div className="wrapper flex flex-row justify-evenly items-center gap-[30px]">
+            <div className="flex justify-center items-center h-24">
+              <Image
+                alt="Logo dae Uniclube"
+                src="/images/Logo.png"
+                height={100}
+                width={220}
+              />
+            </div>
+            <div className="flex justify-center items-center h-24 max-w-[620px] w-[620px]">
+              <Input
+                name="pesquisa"
+                placeholder="O que você gostaria hoje?"
+                onChange={(e) => setSearch(e.target.value)}
+                value={search}
+              />
+            </div>
+            <div className="flex justify-center items-center h-24">
+              <CircleButton
+                icon={FiMapPin}
+                sizeIcon={20}
+                size={36}
+                text={textCep}
+                onClick={cepActionButton}
+                bgColor="#F7F3EE"
+                colorIcon="#F47920"
+              />
+            </div>
+            <div className="flex justify-center items-center h-24">
+              <CircleButton
+                icon={BiUser}
+                sizeIcon={20}
+                size={36}
+                text={textLogin}
+                onClick={cepActionButton}
+                bgColor="#F7F3EE"
+                colorIcon="#F47920"
+              />
+            </div>
+            <div className="flex justify-center items-center h-24">
+              <CircleButton
+                icon={LuShoppingCart}
+                sizeIcon={20}
+                size={36}
+                onClick={cepActionButton}
+                bgColor="#F7F3EE"
+                colorIcon="#F47920"
+              />
+            </div>
           </div>
-          <div className="flex justify-center items-center h-24 max-w-[572px] w-[572px]">
-            <Input
-              name="pesquisa"
-              placeholder="O que você gostaria hoje?"
-              onChange={(e) => setSearch(e.target.value)}
-              value={search}
-            />
-          </div>
-          <div className="flex justify-center items-center h-24">
-            <CircleButton
-              icon={FiMapPin}
-              sizeIcon={20}
-              size={36}
-              text={textCep}
-              onClick={cepActionButton}
-              bgColor="#F7F3EE"
-              colorIcon="#F47920"
-            />
-          </div>
-          <div className="flex justify-center items-center h-24">
-            <CircleButton
-              icon={BiUser}
-              sizeIcon={20}
-              size={36}
-              text={textLogin}
-              onClick={cepActionButton}
-              bgColor="#F7F3EE"
-              colorIcon="#F47920"
-            />
-          </div>
-          <div className="flex justify-center items-center h-24">
-            <CircleButton
-              icon={LuShoppingCart}
-              sizeIcon={20}
-              size={36}
-              onClick={cepActionButton}
-              bgColor="#F7F3EE"
-              colorIcon="#F47920"
-            />
+        </div>
+        <div className="bg-[#F7F3EE] h-[60px] w-full flex justify-center">
+          <div className="wrapper flex flex-row items-center justify-between px-4 h-full mx-[279px]">
+            <div className="flex flex-row items-center">
+              <NavBar />
+              <p className="text-[#3F3F3F]">Descontos Clienter United Natal</p>
+            </div>
+            <p className="text-[#3F3F3F]">Venda no uniclube</p>
           </div>
         </div>
       </header>

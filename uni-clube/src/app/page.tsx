8b3279d29bb41506e1174fc8720e1ee467ba80e2
Manrick-Lazaro <manricksantos@gmail.com>
2025-02-06 15:19:46 -0300

    feat: adiciona os componentes das seções na pagina principal

--- uni-clube/src/app/page.tsx
+++ uni-clube/src/app/page.tsx
@@ -1,10 +1,20 @@
+"use client";
+
 import { Banner } from "@/components/banner";
+import { ClientSection } from "@/components/clientSection";
+import { DepartamentBanner } from "@/components/departamentBanner";
 
 export default function Home() {
   return (
     <div className="flex justify-center mt-[30px]">
-      <section className="wrapper flex justify-center items-center">
-        <Banner />
+      <section className="wrapper flex justify-center items-center flex-col">
+        <div className="w-full mb-[40px]">
+          <Banner />
+        </div>
+
+        <DepartamentBanner />
+
+        <ClientSection />
       </section>
     </div>
   );

6861318f300b1b23dafa19584019167d396e2da4
Manrick-Lazaro <manricksantos@gmail.com>
2025-02-05 21:17:21 -0300

    feat: adiciona os botões circulares ao header.

--- uni-clube/src/components/header/index.tsx
+++ uni-clube/src/components/header/index.tsx
@@ -2,15 +2,36 @@
 
 import Image from "next/image";
 import { JSX, useState } from "react";
+import { FiMapPin } from "react-icons/fi";
+import { BiUser } from "react-icons/bi";
+import { LuShoppingCart } from "react-icons/lu";
+
 import { Input } from "../input";
+import { CircleButton } from "../circleButton";
 
 export function Header(): JSX.Element {
   const [search, setSearch] = useState("");
+  const textCep = (
+    <p className="text-left leading-[18px]">
+      <span className="font-semibold">Onde</span>
+      <br />
+      você está?
+    </p>
+  );
+  const textLogin = (
+    <p className="text-left leading-[18px]">
+      <span className="font-semibold">Faça Login</span>
+      <br />
+      ou cadastre-se
+    </p>
+  );
+
+  function cepActionButton(): void {}
 
   return (
     <div>
       <div className="h-[8px] w-full bg-[#F47920]"></div>
-      <header className="h-[122px] bg-[#EAE0D5]">
+      <header className="h-[122px] flex justify-center items-center bg-[#EAE0D5]">
         <div className="wrapper flex flex-row justify-center items-center gap-[30px] ">
           <div className="flex justify-center items-center h-24">
             <Image
@@ -28,8 +49,39 @@ export function Header(): JSX.Element {
               value={search}
             />
           </div>
+          <div className="flex justify-center items-center h-24">
+            <CircleButton
+              icon={FiMapPin}
+              sizeIcon={20}
+              size={36}
+              text={textCep}
+              onClick={cepActionButton}
+              bgColor="#F7F3EE"
+              colorIcon="#F47920"
+            />
+          </div>
+          <div className="flex justify-center items-center h-24">
+            <CircleButton
+              icon={BiUser}
+              sizeIcon={20}
+              size={36}
+              text={textLogin}
+              onClick={cepActionButton}
+              bgColor="#F7F3EE"
+              colorIcon="#F47920"
+            />
+          </div>
+          <div className="flex justify-center items-center h-24">
+            <CircleButton
+              icon={LuShoppingCart}
+              sizeIcon={20}
+              size={36}
+              onClick={cepActionButton}
+              bgColor="#F7F3EE"
+              colorIcon="#F47920"
+            />
+          </div>
         </div>
-        <div className="flex justify-center items-center h-24"></div>
       </header>
     </div>
   );

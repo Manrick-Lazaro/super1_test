9d25a6d5a6acaa090fa1d9423986dac4532525e4
Manrick-Lazaro <manricksantos@gmail.com>
2025-02-06 15:17:11 -0300

    feat: adiciona menu drop down no navbar.

--- uni-clube/src/components/navbar/index.tsx
+++ uni-clube/src/components/navbar/index.tsx
@@ -1,6 +1,9 @@
+"use client";
+
 import Image from "next/image";
 import { useState, JSX } from "react";
 import { FaChevronUp, FaChevronDown } from "react-icons/fa";
+import { DropdownMenu } from "../dropdownMenu";
 
 export function NavBar(): JSX.Element {
   const [menuIsOpen, setMenuIsOpen] = useState(false);
@@ -10,24 +13,27 @@ export function NavBar(): JSX.Element {
   }
 
   return (
-    <nav className="flex flex-row items-center">
-      <button
-        className="flex items-center"
-        onClick={toggleMenu}
-        aria-label="Menu"
-      >
-        <Image src="/svg/icons/menu.svg" alt="Menu" width={19} height={13} />
+    <>
+      <nav className="flex flex-row items-center">
+        <button
+          className="flex items-center"
+          onClick={toggleMenu}
+          aria-label="Menu"
+        >
+          <Image src="/svg/icons/menu.svg" alt="Menu" width={19} height={13} />
+          <p className="bold text-[#3F3F3F] ml-[10px] mr-[8px]">
+            Departamentos
+          </p>
+          {menuIsOpen ? (
+            <FaChevronUp className="w-7 h-13 text-[#F47920]" />
+          ) : (
+            <FaChevronDown className="w-7 h-13 text-[#F47920]" />
+          )}
+        </button>
+        <div className="border-r-[2px] border-[#EAE0D5] mx-4 rounded-full w-1 h-[30px]"></div>
+      </nav>
 
-        <p className="bold text-[#3F3F3F] ml-[10px] mr-[8px]">Departamentos</p>
-
-        {menuIsOpen ? (
-          <FaChevronUp className="w-7 h-13 text-[#F47920]" />
-        ) : (
-          <FaChevronDown className="w-7 h-13 text-[#F47920]" />
-        )}
-      </button>
-
-      <div className="border-r-[2px] border-[#EAE0D5] mx-4 rounded-full w-1 h-[30px]"></div>
-    </nav>
+      <DropdownMenu isOpen={menuIsOpen} onClose={() => setMenuIsOpen(false)} />
+    </>
   );
 }

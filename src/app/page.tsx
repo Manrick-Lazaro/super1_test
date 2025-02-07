f876d481c9ecd94f6a07bb86c27db254e1b7d3cb
Manrick-Lazaro <manricksantos@gmail.com>
2025-02-07 16:29:03 -0300

    fix: redimenciona container principal.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ import { DepartamentBanner } from "@/components/departamentBanner";
 export default function Home() {
   return (
     <div className="flex justify-center mt-[30px]">
-      <section className="wrapper flex justify-center items-center flex-col">
+      <section className="w-[90%] 700:w-[70%] 700:max-w-[1320px] flex justify-center items-center flex-col">
         <div className="w-full mb-[40px]">
           <Banner />
         </div>

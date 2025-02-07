3ea75c715e093f56674d5f9e11a833ed04948b09
Manrick-Lazaro <manricksantos@gmail.com>
2025-02-07 16:27:32 -0300

    fix: ajusta a responsividade do componente Card

--- src/components/clientSection/card.tsx
+++ src/components/clientSection/card.tsx
@@ -14,7 +14,7 @@ export function Card({
   linkSeller,
 }: cardProps): JSX.Element {
   return (
-    <div className="max-w-[250px] min-h-[460px] max-h-[460px] bg-white rounded-md shadow-xl cursor-pointer ">
+    <div className="max-w-[250px] w-full min-h-[370px] max-h-[490px] bg-white rounded-md shadow-xl cursor-pointer ">
       <Image
         src={image}
         alt="imagem da capa do projeto"
@@ -23,9 +23,9 @@ export function Card({
         className="transition duration-500 hover:brightness-50"
       />
 
-      <div className="px-4 py-4 flex flex-col justify-end">
+      <div className="px-4 py-4 flex flex-col justify-end bg-blue-200">
         {tittle !== undefined ? (
-          <h1 className="text-[#3F3F3F] text-[18px] max-h-[60px] min-h-[60px] mb-1 overflow-hidden text-ellipsis ">
+          <h1 className="text-[#3F3F3F] text-sm 990:text-lg h-[60px] mb-1 overflow-hidden text-ellipsis ">
             {tittle}
           </h1>
         ) : (
@@ -33,10 +33,10 @@ export function Card({
         )}
 
         <div className="flex flex-col justify-end ">
-          <div className="leading-none mb-[2px] flex flex-row items-center gap-[7px]">
+          <div className="text-xs 990:text-sm leading-none mb-[2px] flex flex-row items-center gap-[7px]">
             {promotion !== undefined ? (
               <>
-                <span className="text-[14px] text-[#737373] line-through m-0">
+                <span className="text-[#737373] line-through m-0">
                   R$ {PriceLocale(promotion.oldPrice)}
                 </span>
                 <span className="text-[#CC0A12] text-[12px]">
@@ -45,18 +45,18 @@ export function Card({
               </>
             ) : (
               <>
-                <span className="text-[14px] text-[#737373] line-through m-0">
+                <span className="text-[#737373] line-through m-0">
                   A partir de
                 </span>
               </>
             )}
           </div>
-          <span className="font-bold text-[26px] text-[#3F3F3F] leading-none mb-[10px]">
+          <span className="font-bold text-lg 990:text-2xl text-[#3F3F3F] leading-none mb-[10px]">
             R$ {PriceLocale(price)}
           </span>
         </div>
 
-        <div className="mb-[15px] h-[72px]">
+        <div className="mb-[15px] h-[72px] overflow-auto">
           <p className="text-[#737373]">
             {description.creditCard !== undefined ? (
               <>
@@ -84,7 +84,7 @@ export function Card({
         </div>
 
         <div>
-          <p className="text-[#737373] overflow-hidden text-ellipsis whitespace-nowrap">
+          <p className="text-xs 990:text-sm text-[#737373] overflow-hidden text-ellipsis whitespace-nowrap">
             Vendido por{" "}
             <a className="underline" href={linkSeller}>
               {seller}

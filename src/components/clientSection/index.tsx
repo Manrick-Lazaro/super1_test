9abc53360b96d234602ae54a614bedb1cade4870
Manrick-Lazaro <manricksantos@gmail.com>
2025-02-07 15:01:33 -0300

    fix: melhora a responsividade em sectionClient

--- src/components/clientSection/index.tsx
+++ src/components/clientSection/index.tsx
@@ -3,18 +3,18 @@ import { Card } from "./card";
 
 export function ClientSection(): JSX.Element {
   return (
-    <div className="relative w-[98%] min-h-0 rounded-md mt-[30px]">
-      <div className="bg-gradient-to-b from-[#00995D] via-[#00995D] to-[#59B650] rounded-md h-fit">
+    <div className="relative w-full min-h-0 rounded-md mt-[30px]">
+      <div className="bg-gradient-to-b from-[#00995D] via-[#00995D] to-[#59B650] rounded-md h-fit w-full">
         <div
           className="bg-center bg-cover bg-no-repeat rounded-md "
           style={{ backgroundImage: "url('/images/background.png')" }}
         >
-          <section className="flex flex-col py-[40px] px-[50px] rounded-md">
-            <h1 className="text-white text-[30px] mb-[30px]">
+          <section className="flex flex-col items-start py-7 px-3 990:px-6 rounded-md">
+            <h1 className="text-white text-[18px] 700:text-[20px] 990:text-[30px] mb-[30px]">
               Descontos cliente <span className="font-bold">Unimed Natal</span>
             </h1>
 
-            <div className="grid grid-cols-1 700:grid-cols-2 900:grid-cols-3 1200:grid-cols-4 1400:grid-cols-5 gap-4 ">
+            <div className="grid grid-cols-2 590:grid-cols-3 900:grid-cols-3 1200:grid-cols-4 1400:grid-cols-5 gap-4">
               <div className="col-span-1">
                 <Card
                   image={"/images/artesanato-potiguar 1.png"}

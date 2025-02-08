8acfd8174731b21f56a257376e8051c4a0da42ef
Manrick-Lazaro <manricksantos@gmail.com>
2025-02-08 13:28:49 -0300

    fix: ajuste no uso das props de tamanho no coponente input.

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -6,11 +6,16 @@ import { IoSearch } from "react-icons/io5";
 
 export function Input(props: InputProps): JSX.Element {
   return (
-    <div className={`relative w-[${props.w}] h-[${props.h}]`}>
+    <div
+      className="relative w-full"
+      style={{
+        height: props.h !== undefined ? `${props.h}px` : 54,
+        width: props.w !== undefined ? `${props.w}px` : "100%",
+      }}
+    >
       <input
         {...props}
-        className="rounded-[30px]  w-full focus:outline-none focus:ring-1 pl-5 drop-shadow-md pr-12"
-        style={{ height: props.h ?? 54, width: props.w ?? "100%" }}
+        className="rounded-[30px] w-full h-full focus:outline-none focus:ring-1 pl-5 drop-shadow-md pr-12"
       />
       <IoSearch
         className={`mb-2 text-gray-400 w-6 h-6 absolute right-5 top-1/2 transform -translate-y-1/2 `}

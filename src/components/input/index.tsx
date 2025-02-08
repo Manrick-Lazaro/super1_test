9d9471a91010d722150c40a3edb3494f171232d6
Manrick-Lazaro <manricksantos@gmail.com>
2025-02-08 13:02:19 -0300

    feat: implementa prop no componente input para manipular tamanho.

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -6,12 +6,15 @@ import { IoSearch } from "react-icons/io5";
 
 export function Input(props: InputProps): JSX.Element {
   return (
-    <div className="relative w-full">
+    <div className={`relative w-[${props.w}] h-[${props.h}]`}>
       <input
         {...props}
-        className="rounded-[30px] h-[54px] w-full focus:outline-none focus:ring-1 pl-5 drop-shadow-md"
+        className="rounded-[30px]  w-full focus:outline-none focus:ring-1 pl-5 drop-shadow-md pr-12"
+        style={{ height: props.h ?? 54, width: props.w ?? "100%" }}
       />
-      <IoSearch className="mb-2 text-gray-400 w-6 h-6 absolute right-5 top-4 " />
+      <IoSearch
+        className={`mb-2 text-gray-400 w-6 h-6 absolute right-5 top-1/2 transform -translate-y-1/2 `}
+      />
     </div>
   );
 }
